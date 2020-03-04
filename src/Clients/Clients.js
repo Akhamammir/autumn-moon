c5277ede8c5ce18c51f99c92d164c14903e684bb
Akhamammir <it@paseusted.com.mx>
2020-03-03 18:13:50 -0600

    Primer pase a noyola

--- src/Clients/Clients.js
+++ src/Clients/Clients.js
@@ -2,11 +2,13 @@ import React from 'react';
 //import './User.css';
 import UsrBar from './../UsrBar/UsrBar';
 import NavBar from './../NavBar/NavBar';
+import './Clients.css'
 import DecoratedInput from './../Components/DecoratedInput/DecoratedInput';
 import DecoratedSelect from './../Components/DecoratedSelect/DecoratedSelect';
 import DecoratedCalendar from './../Components/DecoratedCalendar/DecoratedCalendar';
 import { Grommet, Box, Grid, Heading } from 'grommet';
-import { Table, Avatar, Icon, Button, Modal, Alert, Progress  } from 'rsuite';
+import { Table, Toggle, Icon, Button, Modal, Input, IconButton, 
+  Alert, Progress, List, Steps } from 'rsuite';
 import axios from 'axios';
 const { Line } = Progress;
 const { Column, HeaderCell, Cell, Pagination } = Table;
@@ -16,8 +18,17 @@ class ClientReg extends React.Component {
     super(props);
     console.log(props)
     this.state = {usr:this.props.location.state, usrlist:[],
-      name:'', nameFather:'', nameMother:'', birthday:'', gender:'', curp:'', rfc:'',
-    dateHire:'', position:'', phoneNum:'', emergencyNum:'', academic:'', password:'', "_id":'', show:false}
+      name:'', nameFather:'', nameMother:'', birthday:'', gender:'',
+      curp:'', rfc:'', Pphys: false, Branch:false, Step:0, Extran:false, 
+      dateHire:'', position:'', phoneNum:'', emergencyNum:'', academic:'',
+      password:'', "_id":'', show:false,
+      BranchAdd:[
+        'Roses are red',
+        'Violets are blue',
+        'Sugar is sweet',
+        'And so are you',
+       ]
+    }
   }
   componentDidMount() {
     axios.post('http://35.232.231.98:3001/userslist', {team:this.state.usr.Team}).then(res => {
@@ -74,6 +85,9 @@ class ClientReg extends React.Component {
   }
   swap = () =>{
     this.setState({ show: !this.state.show })
+  }
+  forward = () =>{
+    this.setState({Step:this.state.Step+1})
   }
   render() {
     return (
@@ -137,17 +151,21 @@ class ClientReg extends React.Component {
                 { name: 'info', start: [1, 1], end: [1,1] }
               ]}
             >
-              <Box gridArea="avatar">
-                
-              </Box>
               <Box gridArea="name">
+                <Steps current={this.state.Step}>
+                  <Steps.Item title="Datos Generales" />
+                  <Steps.Item title="Datos de Contacto" />
+                  <Steps.Item title="Car. Fiscales" />
+                  <Steps.Item title="Documentacion" />
+                </Steps>
+                <br/>
                 <Heading
                   level="2"
                   margin="none"
                   style = {{
                     left:"50%",
-                    color: "#323435"
                   }}
+                  className = "GreenLetter"
                   textAlign="center"
                 >
                   { this.state.name[0] ? (this.state.name + ' ' + this.state.nameFather + ' ' + this.state.nameMother) 
@@ -156,131 +174,184 @@ class ClientReg extends React.Component {
                 <br/>
                 <Heading level="3" margin="none" style = {{left:"60%", marginLeft:"20px"}} textAlign="center">{this.state.curPos}</Heading>
                 <br/>
-                
               </Box>
-              <Box gridArea="info">
-              <Box direction="row">
-                  <DecoratedInput
-                    area="Razon"
-                    value={this.state.name}
-                    onChange={ (e) => {this.handleChange(e, 'name') } }
-                    width = "100%"
-                    boxw = "90px"
-                    textw = "medium"
-                    icon = "id-mapping"
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                  <DecoratedInput
-                    area="Nombre Comercial"
-                    value={this.state.nameMother}
-                    onChange={ (e) => {this.handleChange(e, 'nameMother') } }
-                    width = "100%"
-                    boxw = "170px"
-                    textw = "medium"
-                    icon = "id-mapping"
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                  <DecoratedInput
-                    area="Domicilio Fiscal"
-                    value = {this.state.Fiscal}
-                    width = "100%"
-                    boxw = "140px"
-                    textw = "medium"
-                    icon = "hourglass-2"
-                    display = {true}
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                <DecoratedInput
-                    area="Curp"
-                    value={this.state.curp}
-                    onChange={ (e) => {this.handleChange(e, 'curp') } }
-                    width = "60%"
-                    icon = "id-card"
-                  />
-                  <DecoratedInput
-                    area="RFC"
-                    value={this.state.rfc}
-                    onChange={ (e) => {this.handleChange(e, 'rfc') } }
-                    width = "60%"
-                    icon = "id-card"
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                  <DecoratedCalendar
-                    area="Fecha Ing."
-                    value={this.state.dateHire}
-                    onChange={ (e) => {this.handleChange(e, 'dateHire') } }
-                    width = "60%"
-                    icon = "Gift"
-                  />
-                  <DecoratedInput
-                    area="Puesto"
-                    value={this.state.position}
-                    onChange={ (e) => {this.handleChange(e, 'position') } }
-                    width = "60%"
-                    icon = "peoples-map"
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                  <DecoratedInput
-                    area="Numero Tel."
-                    value={this.state.phoneNum}
-                    onChange={ (e) => {this.handleChange(e, 'phoneNum') } }
-                    width = "60%"
-                    type="number"
-                    icon = "mobile"
-                  />
-                  <DecoratedInput
-                    area="Tel. Emer"
-                    value={this.state.emergencyNum}
-                    onChange={ (e) => {this.handleChange(e, 'emergencyNum') } }
-                    width = "60%"
-                    type="number"
-                    icon = "phone"
-                  />
-                </Box>
-                <br/>
-                <Box direction="row">
-                  <DecoratedInput
-                    area="Nivel Acad."
-                    value={this.state.academic}
-                    onChange={ (e) => {this.handleChange(e, 'academic') } }
-                    width = "60%"
-                    icon = "mortar-board"
-                  />
-                  <DecoratedInput
-                    area="Password"
-                    onChange={ (e) => {this.handleChange(e, 'password') } }
-                    width = "60%"
-                    icon = "key"
-                    type = "password"
-                  />
-                </Box>
-                <br/>
-                <Button
-                  style={{
-                    backgroundColor:"#6FFFB0",
-                    width:"120px",
-                    fontFamily:"'Manjari', sans-serif",
-                    boxShadow: "0px 2px 4px rgba(0,0,0,0.20)"
-                  }}
-                  onClick = { () => this.handleUpload() }
-                >
-                  <Icon icon="user-plus"  />&nbsp;&nbsp;Add User
-                </Button>
-              </Box>
+              { this.state.Step === 0 ? <this.StepOne/> : <span></span> }
+              { this.state.Step === 1 ? <this.StepTwo/> : <span></span> }
             </Grid>
           </Box>
         </Grid>
       </Grommet>
+    );
+  }
+  StepOne = () => {
+    return(
+      <Box gridArea="info">
+      <Box direction="row">
+          <DecoratedInput
+            area="Razon"
+            value={this.state.name}
+            onChange={ (e) => {this.handleChange(e, 'name') } }
+            width = "85%"
+            boxw = "90px"
+            textw = "medium"
+            icon = "id-mapping"
+          />
+          <Toggle 
+            size="lg"
+            checkedChildren="P. Moral"
+            unCheckedChildren="P. Fisica"
+            checked= {this.state.Pphys}
+            onChange = { () => { this.setState({Pphys:!this.state.Pphys}) } }
+          />
+        </Box>
+        <br/>
+        <Box direction="row">
+          <DecoratedInput
+            area="Nombre Comercial"
+            value={this.state.nameMother}
+            onChange={ (e) => {this.handleChange(e, 'nameMother') } }
+            width = "100%"
+            boxw = "170px"
+            textw = "medium"
+            icon = "id-mapping"
+          />
+        </Box>
+        <br/>
+        <Box direction="row">
+          <DecoratedInput
+            area="Domicilio Fiscal"
+            value = {this.state.Fiscal}
+            width = "100%"
+            boxw = "140px"
+            textw = "medium"
+            icon = "hourglass-2"
+          />
+        </Box>
+        <br/>
+        <Box direction="row">
+        { !this.state.Pphys ? 
+        <DecoratedInput
+            area="Curp"
+            value={this.state.curp}
+            onChange={ (e) => {this.handleChange(e, 'curp') } }
+            width = "60%"
+            icon = "id-card"
+            display = { this.state.Pphys }
+          />
+          : 
+          <DecoratedInput
+            area="RINE"
+            value={this.state.position}
+            onChange={ (e) => {this.handleChange(e, 'position') } }
+            width = "60%"
+            icon = "peoples-map"
+          />
+        }
+          <DecoratedInput
+            area="RFC"
+            value={this.state.rfc}
+            onChange={ (e) => {this.handleChange(e, 'rfc') } }
+            width = "60%"
+            icon = "id-card"
+          />
+        </Box>
+        <br/>
+        <Box direction="row">
+          
+          <DecoratedInput
+            area="Numero Tel."
+            value={this.state.phoneNum}
+            onChange={ (e) => {this.handleChange(e, 'phoneNum') } }
+            width = "60%"
+            type="number"
+            icon = "mobile"
+          />
+        </Box>
+        <br/>
+        <Box>
+            <Box direction="row">
+              <Heading
+                margin="small"
+                level={5}
+                className = "GreenLetter"
+              >
+                El cliente cuenta con sucursales?
+              </Heading>
+              <Toggle
+                size="lg"
+                checkedChildren="Si"
+                unCheckedChildren="No"
+                className = "PushDown"
+                checked= {this.state.Branch}
+                onChange = { () => { this.setState({Branch:!this.state.Branch}) } }
+              />
+            </Box>
+            {this.state.Branch ? 
+              <Box>
+                <List hover className = 'ListColor'>
+                  {this.state.BranchAdd.map((item,index)=>
+                    <List.Item key={index} index={index} className="Pad">
+                      <Box direction="row">
+                        <Input className="ListInput" placeholder="Col." />
+                        <Input className="ListInput" placeholder="Calle y Numero" />
+                        <Input className="ListInput" placeholder="C.P." />
+                        <Input className="ListInput" placeholder="Estado" />
+                      </Box>
+                    </List.Item>
+                  )}
+                </List>
+                <br/>
+                <IconButton icon={<Icon icon="plus" />} circle size="md" className="" />
+              </Box> 
+            : 
+              <span></span>
+            }
+            <br/>
+            <br/>
+          </Box>
+        { !this.state.Pphys ? 
+          <span></span>
+          :
+          <Box>
+            <Box direction="row">
+              <Heading
+                margin="small"
+                level={5}
+                className = "GreenLetter"
+              >
+                El cliente cuenta con socios en el extranejro?
+              </Heading>
+              <Toggle
+                size="lg"
+                checkedChildren="Si"
+                unCheckedChildren="No"
+                className = "PushDown"
+                checked= {this.state.Extran}
+                onChange = { () => { this.setState({Extran:!this.state.Extran}) } }
+              />
+            </Box>
+            <br/>
+            <br/>
+          </Box>
+        }
+        <Button
+          style={{
+            backgroundColor:"#6FFFB0",
+            width:"120px",
+            fontFamily:"'Manjari', sans-serif",
+            boxShadow: "0px 2px 4px rgba(0,0,0,0.20)"
+          }}
+          onClick = { () => this.forward() }
+        >
+          Siguiente&nbsp;&nbsp;<Icon icon="hand-o-right"  />
+        </Button>
+      </Box>
+    );
+  }
+  StepTwo = () =>{
+    return(
+      //
+      <span></span>
     );
   }
 }
