dc2a5188f805f0f2f3322680b28692c0eda8d875
Akhamammir <it@paseusted.com.mx>
2020-06-09 08:40:09 -0500

    new login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Login.css';
 import 'rsuite/dist/styles/rsuite-default.css';
-import { Grommet, Button, WorldMap, TextInput, Box, Heading, Text, Anchor, Grid } from 'grommet';
-import { Alert } from 'rsuite';
+import { Grommet, WorldMap, TextInput, Box, Heading, Text, Anchor, Grid } from 'grommet';
+import { Alert, Input, Button } from 'rsuite';
 import axios from 'axios';
 class Login extends React.Component {
   constructor(props) {
@@ -20,65 +20,44 @@ class Login extends React.Component {
     })
   }
   render() {
+    //<img src={logo} className="App-logo" alt="logo" />
     return (
-      <Grommet plain className="App">
+      <div className="App">
         <header className="App-header">
-          <Grid
-            alignContent="center"
-            rows={['70px', '600px', '250px']}
-            columns={['7vw', 'xxlarge', '1px']}
-            gap="small"
-            areas={[
-              { name: 'header', start: [0, 0], end: [2, 0] },
-              { name: 'nav', start: [1, 1], end: [1, 1] },
-              { name: 'main', start: [0, 2], end: [2, 2] },
-            ]}
+        <h3>Heza Consultorias SC</h3>
+        <br/>
+        <Input
+          style={{ width: 300 }}
+          placeholder="Usuario"
+          className="inputLog"
+          value={this.state.usr}
+          onChange={(e) => this.setState({ usr: e.target.value })}
+        />
+        <br/>
+        <Input
+          style={{ width: 300 }}
+          placeholder="Contraseña"
+          className="inputLog"
+          value={this.state.pwd}
+          onChange={(e) => this.setState({ pwd: e.target.value })}
+        />
+        <br/>
+        <Button appearance="primary" onClick={this.Login} >Entrar</Button>
+        <br/>
+          <p>
+            Este software se alimenta de:
+            <br/>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <Box gridArea="header">
-              <Heading color='accent-1' size="large" id="Fonter" alignSelf="center">Heza</Heading>
-            </Box>
-            <WorldMap
-              fill="vertical"
-              margin={{left:'6vw'}}
-              gridArea="nav"
-              alignSelf="center"
-              color="status-ok"
-              continents={[
-                {
-                  name: 'North America',
-                  color: 'accent-4',
-                },
-              ]}
-              onSelectPlace={(lat, lon) => { }}
-              places={[
-                {
-                  name: 'Puerto Vallarta',
-                  location: [20.685601, -105.229814],
-                  color: 'status-critical',
-                },
-              ]}
-              selectColor="accent-2"
-            />
-            <Box gridArea="main" alignContent="center">
-              <Box width="medium" alignSelf="center">
-                <TextInput placeholder="Username" value={this.state.usr} onChange={(e) => this.setState({ usr: e.target.value })} size="small" />
-              </Box>
-              <br />
-              <Box width="medium" alignSelf="center">
-                <TextInput placeholder="Password" value={this.state.pwd} onChange={(e) => this.setState({ pwd: e.target.value })} type="password" size="small" styleClass="half-wide" />
-              </Box>
-              <br />
-              <Box width="medium" alignSelf="center">
-                <Button label='Submit' onClick={this.Login} color="accent-1" primary />
-              </Box>
-              <br />
-              <Box width="medium" alignSelf="center">
-                <Text>This Software is Powered By Eating </Text><Anchor href="https://www.cookiies.dev" color="accent-1" label="Cookies" />
-              </Box>
-            </Box>
-          </Grid>
+            Cookies
+          </a>
+          </p>
         </header>
-      </Grommet>
+      </div>
     );
   }
 }
